5e5e7db82023d3048942b153dee0da076a800d43
teb <85996160+Teebak@users.noreply.github.com>
2024-03-06 22:36:06 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+const newData = {
+    name: username,
+    email: useremail
+}
+
+
+return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+    },
+    body: JSON.stringify(newData)
+})
+.then(resp => resp.json())
+.then(object => {
+    let h3 = document.createElement('h3')
+    h3.textContent = object.id
+    document.body.appendChild(h3)
+})
+.catch(error => {
+    let h4 = document.createElement('h4')
+    h4.textContent = error.message
+    document.body.appendChild(h4)
+})
+
+ 
